0b0b1d9f0e0f478b8845a9ec28a1379da77be6aa
elliott-ribner <elr886@gmail.com>
2016-03-15 23:30:46 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+	res.send('yep im working bud');
+});
+
+app.post('/incoming', function(request, response) {
+	response.send('<Response>This is my next message</Response>')
+});
+
+app.listen(3000, function() {
+	console.log('app is runnning on port 3000');
+})
