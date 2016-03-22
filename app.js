9a9a3894c617549de3fd2bcd3b4cff977fdf72b3
elliott-ribner <elr886@gmail.com>
2016-03-21 20:47:10 -0400

    -

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://localhost:27017/rubens';
+
 var express = require('express');
 var app = express();
+
 
 app.get('/', function(req, res) {
 	res.send('yep im working bud');
@@ -7,10 +13,30 @@ app.get('/', function(req, res) {
 
 app.post('/incoming', function(request, response) {
 	console.log(request);
+	
 	response.header('Content-Type', 'text/xml');
 	response.send('<Response><Sms>This is my next message</Sms></Response>')
 });
 
 app.listen(process.env.PORT || 3000, function() {
 	console.log('app is runnning on port 3000');
+})
+
+
+var insertDocument = function(db, callback) {
+	db.collection('users').insertOne({
+		"phoneNumber":"9875756453",
+		"step": 0
+	}, function(err, result) {
+		assert.equal(err,null);
+		console.log("inserted a document");
+		callback();
+	});
+}
+
+MongoClient.connect(url, function(err,db) {
+	assert.equal(null, err);
+	insertDocument(db, function() {
+		db.close();
+	});	
 })
---
+++ conversation.js
@@ -0,0 +1,8 @@
+
+class IncomingMessage {
+	constructor(number, message) {
+		this.number = number;
+		this.message = message;
+	}
+}
+
