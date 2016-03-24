715e048a122b1bc5bc0514b4657dc23d0acb5a97
elliott-ribner <elr886@gmail.com>
2016-03-23 22:22:55 -0400

    mongolab

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-var ObjectId = require('mongodb').ObjectID;
-var url = 'mongodb://localhost:27017/rubens';
-
 var express = require('express');
 var app = express();
+
+//new
+var mongoose = require('mongoose');
+//mongo creds coming from setup as described in https://devcenter.heroku.com/articles/mongolab - heroku addons create
+mongoose.connect('mongodb://heroku_fbn116f1:g4t312kspun05da14eisj94srl@ds045064.mlab.com:45064/heroku_fbn116f1');
+
+var User = mongoose.model('User', {phone: String, step: Number});
+var user1 = new User({phone: "8889997171", step: 2});
+
+user1.save(function(err,userObj) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('saved successfully:', userObj);
+	}
+});
+
 
 
 app.get('/', function(req, res) {
@@ -13,7 +25,20 @@ app.get('/', function(req, res) {
 
 app.post('/incoming', function(request, response) {
 	console.log(request);
-	
+	console.log('and response');
+	console.log(response);
+
+	var User = mongoose.model('User', {phone: String, step: Number});
+	var user1 = new User({phone: "8888888888", step: 2});
+
+	user1.save(function(err,userObj) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('saved successfully:', userObj);
+		}
+	});
+
 	response.header('Content-Type', 'text/xml');
 	response.send('<Response><Sms>This is my next message</Sms></Response>')
 });
@@ -23,20 +48,3 @@ app.listen(process.env.PORT || 3000, function() {
 })
 
 
-var insertDocument = function(db, callback) {
-	db.collection('users').insertOne({
-		"phoneNumber":"9875756453",
-		"step": 0
-	}, function(err, result) {
-		assert.equal(err,null);
-		console.log("inserted a document");
-		callback();
-	});
-}
-
-MongoClient.connect(url, function(err,db) {
-	assert.equal(null, err);
-	insertDocument(db, function() {
-		db.close();
-	});	
-})
