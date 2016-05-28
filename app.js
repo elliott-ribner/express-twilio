bc2ec2bfaf743513e256b9632b50c8cd2e7d88b6
elliott-ribner <elr886@gmail.com>
2016-05-28 15:00:24 -0400

    more allow headers

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var bcrypt = require('bcrypt');
 
 app.use(function(req,res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   next();
 });
