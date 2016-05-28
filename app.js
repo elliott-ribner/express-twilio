bb70145f610e54b66a78a5a8fdce73c432e306d5
elliott-ribner <elr886@gmail.com>
2016-05-28 13:20:29 -0400

    changed allowed headers again

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 app.use(function(req,res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With,content-type");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   next();
 });
