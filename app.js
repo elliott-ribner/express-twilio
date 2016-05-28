1791511424bb12d988f698137fac3a569ade5513
elliott-ribner <elr886@gmail.com>
2016-05-28 13:36:00 -0400

    app.use cors

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var bcrypt = require('bcrypt');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
+app.use(cors());
 // app.use(function(req,res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Headers", "X-Requested-With,content-type");
