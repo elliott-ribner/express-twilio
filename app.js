364506b7c6e5c5965465732c3af7a80b673e1088
elliott-ribner <elr886@gmail.com>
2016-05-28 15:07:31 -0400

    status instead of send

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var allowCrossDomain = function(req, res, next) {
       res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
       res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-      res.send(200);
+      res.status(200);
     }
     else {
       next();
