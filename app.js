82f80b89270e601f95dad0005f94a43b756a867e
elliott-ribner <elr886@gmail.com>
2016-05-28 14:46:19 -0400

    logging request

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.set('secret', config.secret);
 
 
 apiRoutes.post('/newuser', function(req,res) {
-
+  console.log('request is',req);
   console.log('body is',req.body);
   var hash = bcrypt.hashSync(req.body.password, 10);
   var admin = new AdminUser({email: req.body.email, password: hash});
