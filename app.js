90ade2fe86db0e04c80c8a38526d55f9cd4e368a
elliott-ribner <elr886@gmail.com>
2016-05-28 12:38:59 -0400

    added one more header allowance

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(morgan('dev'));
 app.use(function(req,res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   next();
 });
 
