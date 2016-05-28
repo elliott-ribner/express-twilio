ae826680124c85a4a4910bacfefbe3235f4d1613
elliott-ribner <elr886@gmail.com>
2016-05-28 15:13:45 -0400

    different send syntax

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var allowCrossDomain = function(req, res, next) {
       res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
       res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-      return res.status(200);
+      return res.status(200).send({success: true});
     }
     else {
       next();
